8effb58ff6e46b847945fea787e7185cd7917577
wyman <voyagingmk@gmail.com>
2018-07-09 14:32:49 +0800

    no message

--- find/binaryFindMin.cpp
+++ find/binaryFindMin.cpp
@@ -4,10 +4,13 @@
 int MinInOrder(int* numbers, int index1, int index2)
 {
     int result = numbers[index1];
+   // printf("result[%d] %d\n" ,index1, result);
     for(int i = index1 + 1; i <= index2; ++i)
     {
-        if (result > numbers[i])
+        // printf("numbers[i], %d %d\n", i, numbers[i]);
+        if (result > numbers[i]) {
             result = numbers[i];
+        }
     }
     return result;
 }
@@ -28,7 +31,8 @@ int Min(int* numbers, int length)
             break;
         }
         indexMid = (index1 + index2) / 2;
-        //如果下标为indexl、index2和indexMid指向的三个数字相等，
+        printf("%d %d %d\n" ,numbers[index1], numbers[indexMid], numbers[index2]);
+        //如果下标为index1、index2和indexMid指向的三个数字相等，
         //则只能顺序查找
         if(numbers[index1] == numbers[index2]
             && numbers[indexMid] == numbers[index1]) {
@@ -43,7 +47,9 @@ int Min(int* numbers, int length)
 }
 
 int main() {
-    int data[] = {4,5,6,7,8,9,1,2,3};
+    int data[] = {1,2,3,1,1,1,1,1,1};
+    // int data[] = {1,-1,0,1,1,1,1,1,1};
+    // int data[] = {4,5,6,7,8,9,1,2,3};
     int len = sizeof(data) / sizeof(int);
     int minNum = Min(data, len);
     printf("min: %d \n", minNum);
