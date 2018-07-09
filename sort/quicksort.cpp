07b8b76275430cd1ab7651dd562195ffe81f4742
wyman <voyagingmk@gmail.com>
2018-07-09 13:46:43 +0800

    no message

--- sort/quicksort.cpp
+++ sort/quicksort.cpp
@@ -1,7 +1,15 @@
 #include <exception>
 #include <stdio.h>
 
+void PrintArray(int data[], int len) {
+    for(int i = 0; i < len; i++) {
+        printf("%d ", data[i]);
+    }
+    printf("\n");
+}
+
 inline void Swap(int *a, int *b) {
+    printf("%d <=> %d\n", *a, *b);
     int tmp = *a;
     *a = *b;
     *b = tmp;
@@ -16,19 +24,24 @@ int Partition(int data[], int len, int start, int end) {
     if (data == nullptr || len <= 0 || start < 0|| end >= len ){
         throw "Invalid Parameters";
     }
+    printf("=== Partition begin: %d %d %d ===\n", len, start, end);
     int index = RandomInt(start, end);
+    printf("index %d \n", index);
     Swap(&data[index], &data[end]);
+    PrintArray(data, len);
     int small = start - 1;
     for(index = start; index < end; ++index) {
         if(data[index] < data[end]) {
             ++small;
             if(small != index) {
                 Swap(&data[index], &data[small]);
+                PrintArray(data, len);
             }
         }
     }
     ++small;
     Swap(&data[small], &data[end]);
+    PrintArray(data, len);
     return small;
 }
 
@@ -45,13 +58,15 @@ void QuickSort(int data[], int len, int start, int end) {
     }
 }
 
+void QuickSort(int data[], int len) {
+    QuickSort(data, len, 0, len - 1);
+}
+
 int main() {
     int data[] = {2,4,5,1,6,3,7,9,8,0};
     int len = sizeof(data) / sizeof(int);
-    QuickSort(data, len, 0, len - 1);
-    for(int i = 0; i < len; i++) {
-        printf("%d ", data[i]);
-    }
-    printf("\n");
+    PrintArray(data, len);
+    QuickSort(data, len);
+    PrintArray(data, len);
     return 0;
 }
